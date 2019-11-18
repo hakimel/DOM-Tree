1b6661cd74ff2c834c719ba3bfc3c0a7cbd62532
Hakim El Hattab <hakim.elhattab@gmail.com>
2019-11-18 14:21:26 +0100

    remove dated code and styles

--- js/domtree.js
+++ js/domtree.js
@@ -1,134 +1,102 @@
-(function(){
+const width = 500;
+const height = 600;
+const quantity = 150;
+const types = [ 'text', 'select', 'progress', 'meter', 'button', 'radio', 'checkbox' ];
+const greetings = [ 'Joyeuses Fêtes','Felices Fiestas','God Jul','Boas Festas','Mutlu Bayramlar','Sarbatori Fericite','Jie Ri Yu Kuai','Bones Festes','Tanoshii kurisumasu wo','Buone Feste','Happy Holidays', 'Ii holide eximnandi','Frohe Feiertage','Prettige feestdagen','Beannachtaí na Féile','Vesele Praznike','Selamat Hari Raya','Sretni praznici' ];
 
-	var supports3DTransforms =  document.body.style['perspectiveProperty'] !== undefined ||
-								document.body.style['WebkitPerspective'] !== undefined ||
-								document.body.style['MozPerspective'] !== undefined ||
-								document.body.style['msPerspective'] !== undefined ||
-								document.body.style['OPerspective'] !== undefined;
+let tree = document.querySelector( '.tree' ),
+	treeRotation = 0;
 
-	if( !supports3DTransforms ) {
-		alert( 'Your browser doesn\'t support CSS3 3D transforms :/' );
-	}
+tree.style.width = width + 'px';
+tree.style.height = height + 'px';
 
-	function transform( element, value ) {
-		element.style.WebkitTransform = value;
-		element.style.MozTransform = value;
-		element.style.msTransform = value;
-		element.style.OTransform = value;
-		element.style.transform = value;
-	}
+window.addEventListener( 'resize', resize, false );
 
-	var width = 500,
-		height = 600,
-		quantity = 250,
-		types = [ 'text', 'select', 'progress', 'meter', 'button', 'radio', 'checkbox' ],
-		greetings = [ 'Joyeuses Fêtes','Felices Fiestas','God Jul','Boas Festas','Mutlu Bayramlar','Sarbatori Fericite','Jie Ri Yu Kuai','Bones Festes','Tanoshii kurisumasu wo','Buone Feste','Happy Holidays', 'Ii holide eximnandi','Frohe Feiertage','Prettige feestdagen','Beannachtaí na Féile','Vesele Praznike','Selamat Hari Raya','Sretni praznici' ];
+// The tree
+for( var i = 0; i < quantity; i++ ) {
+	let element = null,
+		type = types[ Math.floor( Math.random() * types.length ) ],
+		greeting = greetings[ Math.floor( Math.random() * greetings.length ) ];
 
-	var tree = document.querySelector( '.tree' ),
-		treeRotation = 0;
+	let x = width/2,
+		y = Math.round( Math.random() * height );
 
-	tree.style.width = width + 'px';
-	tree.style.height = height + 'px';
+	let rx = 0,
+		ry = Math.random() * 360,
+		rz = -Math.random() * 15;
 
-	window.addEventListener( 'resize', resize, false );
+	let elemenWidth = 5 + ( ( y / height ) * width / 2 ),
+		elemenHeight = 26;
 
-	// The tree
-	for( var i = 0; i < quantity; i++ ) {
-		var element = null,
-			type = types[ Math.floor( Math.random() * types.length ) ],
-			greeting = greetings[ Math.floor( Math.random() * greetings.length ) ];
-
-		var x = width/2,
-			y = Math.round( Math.random() * height );
-
-		var rx = 0,
-			ry = Math.random() * 360,
-			rz = -Math.random() * 15;
-
-		var elemenWidth = 5 + ( ( y / height ) * width / 2 ),
-			elemenHeight = 26;
-
-		switch( type ) {
-			case 'button':
-				element = document.createElement( 'button' );
-				element.textContent = greeting;
-				element.style.width = elemenWidth + 'px';
-				element.style.height = elemenHeight + 'px';
-				break;
-			case 'progress':
-				element = document.createElement( 'progress' );
-				element.style.width = elemenWidth + 'px';
-				element.style.height = elemenHeight + 'px';
-				if( Math.random() > 0.5 ) {
-					element.setAttribute( 'max', '100' );
-					element.setAttribute( 'value', Math.round( Math.random() * 100 ) );
-				}
-				break;
-			case 'select':
-				element = document.createElement( 'select' );
-				element.setAttribute( 'selected', greeting );
-				element.innerHTML = '<option>' + greetings.join( '</option><option>' ) + '</option>';
-				element.style.width = elemenWidth + 'px';
-				element.style.height = elemenHeight + 'px';
-				break;
-			case 'meter':
-				element = document.createElement( 'meter' );
-				element.setAttribute( 'min', '0' );
+	switch( type ) {
+		case 'button':
+			element = document.createElement( 'button' );
+			element.textContent = greeting;
+			element.style.width = elemenWidth + 'px';
+			element.style.height = elemenHeight + 'px';
+			break;
+		case 'progress':
+			element = document.createElement( 'progress' );
+			element.style.width = elemenWidth + 'px';
+			element.style.height = elemenHeight + 'px';
+			if( Math.random() > 0.5 ) {
 				element.setAttribute( 'max', '100' );
 				element.setAttribute( 'value', Math.round( Math.random() * 100 ) );
-				element.style.width = elemenWidth + 'px';
-				element.style.height = elemenHeight + 'px';
-				break;
-			case 'radio':
-				element = document.createElement( 'input' );
-				element.setAttribute( 'type', 'radio' );
-				if( Math.random() > 0.5 ) element.setAttribute( 'checked', '' );
-				break;
-			case 'checkbox':
-				element = document.createElement( 'input' );
-				element.setAttribute( 'type', 'checkbox' );
-				if( Math.random() > 0.5 ) element.setAttribute( 'checked', '' );
-				break;
-			case 'text':
-			default:
-				element = document.createElement( 'input' );
-				element.setAttribute( 'type', 'text' );
-				element.setAttribute( 'value', greeting );
-				element.style.width = elemenWidth + 'px';
-				element.style.height = elemenHeight + 'px';
-		}
-
-		transform( element, 'translate3d('+x+'px, '+y+'px, 0px) rotateX('+rx+'deg) rotateY('+ry+'deg) rotateZ('+rz+'deg)' );
-
-		tree.appendChild( element );
+			}
+			break;
+		case 'select':
+			element = document.createElement( 'select' );
+			element.setAttribute( 'selected', greeting );
+			element.innerHTML = '<option>' + greetings.join( '</option><option>' ) + '</option>';
+			element.style.width = elemenWidth + 'px';
+			element.style.height = elemenHeight + 'px';
+			break;
+		case 'meter':
+			element = document.createElement( 'meter' );
+			element.setAttribute( 'min', '0' );
+			element.setAttribute( 'max', '100' );
+			element.setAttribute( 'value', Math.round( Math.random() * 100 ) );
+			element.style.width = elemenWidth + 'px';
+			element.style.height = elemenHeight + 'px';
+			break;
+		case 'text':
+		default:
+			element = document.createElement( 'input' );
+			element.setAttribute( 'type', 'text' );
+			element.setAttribute( 'value', greeting );
+			element.style.width = elemenWidth + 'px';
+			element.style.height = elemenHeight + 'px';
 	}
 
-	// The snow
-	for( var i = 0; i < 200; i++ ) {
-		var element = document.createElement( 'input' );
-		element.setAttribute( 'type', 'radio' );
+	element.style.transform = `translate3d(${x}px, ${y}px, 0px) rotateX(${rx}deg) rotateY(${ry}deg) rotateZ(${rz}deg)`;
 
-		var spread = width * 2;
+	tree.appendChild( element );
+}
 
-		var x = Math.round( Math.random() * spread ) - ( spread / 4 ),
-			y = Math.round( Math.random() * height ),
-			z = Math.round( Math.random() * spread ) - ( spread / 2 );
+// Let it snow
+for( var i = 0; i < 200; i++ ) {
+	let element = document.createElement( 'input' );
+	element.setAttribute( 'type', 'radio' );
 
-		var rx = 0,
-			ry = Math.random() * 360,
-			rz = 0;
+	let spread = window.innerWidth/2;
 
-		if( Math.random() > 0.5 ) element.setAttribute( 'checked', '' );
+	let x = Math.round( Math.random() * spread ) - ( spread / 4 ),
+		y = Math.round( Math.random() * height ),
+		z = Math.round( Math.random() * spread ) - ( spread / 2 );
 
-		transform( element, 'translate3d('+x+'px, '+y+'px, '+z+'px) rotateX('+rx+'deg) rotateY('+ry+'deg) rotateZ('+rz+'deg)' );
+	let rx = 0,
+		ry = Math.random() * 360,
+		rz = 0;
 
-		tree.appendChild( element );
-	}
+	if( Math.random() > 0.5 ) element.setAttribute( 'checked', '' );
 
-	function resize() {
-		tree.style.top = ( ( window.innerHeight - height - 100 ) / 2 ) + 'px';
-	}
+	element.style.transform = `translate3d(${x}px, ${y}px, ${z}px) rotateX(${rx}deg) rotateY(${ry}deg) rotateZ(${rz}deg)`;
 
-	resize();
+	tree.appendChild( element );
+}
 
-})()
+function resize() {
+	tree.style.top = ( ( window.innerHeight - height - 100 ) / 2 ) + 'px';
+}
+
+resize();
